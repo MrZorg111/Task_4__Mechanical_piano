c87f054e4b815418f581fdcced4787af6e3f9fc3
MrZorg111 <yodji333@mail.ru>
2022-04-23 16:36:02 +0300

    Final Solution

--- main.cpp
+++ main.cpp
@@ -1,71 +1,116 @@
 #include <iostream>
 #include <string>
-#include <cstdlib>
 
-/*
- * Создайте упрощённую модель механического пианино.
- * Всего у данного пианино семь клавиш, которые соответствуют семи нотам.
- * Каждая клавиша кодируется уникальной битовой маской, которая записывается в enum.
- * В начале программы пользователь вводит мелодию.
- * Ввод осуществляется с помощью цифр на клавиатуре, от 1 до 7 включительно (от ноты до до ноты си).
- * В результате из чисел может быть составлена любая комбинация нот. К примеру 512 или 154.
- * После ввода комбинации вводится следующая. Всего таких комбинаций нот — 12.
- * Как только все комбинации были введены пользователем, мелодия проигрывается.
- * Для этого каждая комбинация нот последовательно выводится на экран.
- * Однако на этот раз она печатается «красиво», то есть все ноты указываются словами, а не цифрами.
- * При этом слова разделяются пробелами.
- * Рекомендации
- * Для вычленения отдельных символов-цифр из строки с нотами используйте оператор индексации строки.
- * Для дальнейшей конвертации символа в строку используйте соответствующий конструктор.
- * Для простоты, чтобы из индекса ноты получить саму ноту-флаг из enum, используйте оператор сдвига.
- * Общая формула такова: 1 << индекс ноты (начинается с нуля).
- * К примеру, 1 << 0 — битовый флаг ноты до, 1 << 6 — битовая маска ноты си.
- */
 enum note
 {
     DO = 1,
     RE = 2,
-    MI = 3,
-    FA = 4,
-    SOL = 5,
-    LA = 6,
-    SI = 7
+    MI = 4,
+    FA = 8,
+    SOL = 16,
+    LA = 32,
+    SI = 64
 };
 
 int main() {
-    int melody[12];
-    std::cout << "Enter the note code from 1 to 7: \n";
-    for( int i = 0; i < 12; i++) {
-        std::cin >> melody[i];
+    char note_c;
+    int notes, note_i, melody_1[12], melody_2[12], melody_3[12];
+    std::string melody, accord;
+    std::cout << "Enter the note code from 0 to 6: \n";
+    std::cin >> melody;
+
+    for (int i = 0, t = 0, j = 0; i < melody.length(); i++) {
+        note_c = melody[i];
+        note_i = note_c - '0';
+
+        if (i < 12) {
+            melody_1[i] = note_i;
+        }
+        if (i >= 12 && i < 24) {
+            melody_2[t] = note_i;
+            t++;
+        }
+        if (i >= 24 && i < 36) {
+            melody_3[j] = note_i;
+            j++;
+        }
     }
-
+   std::cout << "The first melody:";
     for(int l = 0; l < 12; l++) {
-        if (melody[l] == DO) {
-            std::cout << "DO";
+        notes = 1 << melody_1[l];
+        if (notes & DO) {
+            std::cout << "DO" << " ";
         }
-        if (melody[l] == RE) {
-            std::cout << "RE";
+        if (notes & RE) {
+            std::cout << "RE" << " ";
         }
-        if (melody[l]== MI) {
-            std::cout << "MI";
+        if (notes & MI) {
+            std::cout << "MI" << " ";
         }
-        if (melody[l] == FA) {
-            std::cout << "FA";
+        if (notes & FA) {
+            std::cout << "FA" << " ";
         }
-        if (melody[l] == SOL) {
-            std::cout << "SOL";
+        if (notes & SOL) {
+            std::cout << "SOL" << " ";
         }
-        if (melody[l] == LA) {
-            std::cout << "LA";
+        if (notes & LA) {
+            std::cout << "LA" << " ";
         }
-        if (melody[l] == SI) {
-            std::cout << "SI";
+        if (notes & SI) {
+            std::cout << "SI" << " ";
         }
-        if (l == 2 || 5 || 8 ) {
-            std::cout << " ";
         }
-
-
+    notes = 0;
+    std::cout << "\nThe second melody:";
+    for(int j = 0; j < 12; j++) {
+        notes = 1 << melody_2[j];
+        if (notes & DO) {
+            std::cout << "DO" << " ";
         }
+        if (notes & RE) {
+            std::cout << "RE" << " ";
+        }
+        if (notes & MI) {
+            std::cout << "MI" << " ";
+        }
+        if (notes & FA) {
+            std::cout << "FA" << " ";
+        }
+        if (notes & SOL) {
+            std::cout << "SOL" << " ";
+        }
+        if (notes & LA) {
+            std::cout << "LA" << " ";
+        }
+        if (notes & SI) {
+            std::cout << "SI" << " ";
+        }
+    }
+    notes = 0;
+    std::cout << "\nThe third melody:";
+    for(int y = 0; y < 12; y++) {
+        notes = 1 << melody_3[y];
+        if (notes & DO) {
+            std::cout << "DO" << " ";
+        }
+        if (notes & RE) {
+            std::cout << "RE" << " ";
+        }
+        if (notes & MI) {
+            std::cout << "MI" << " ";
+        }
+        if (notes & FA) {
+            std::cout << "FA" << " ";
+        }
+        if (notes & SOL) {
+            std::cout << "SOL" << " ";
+        }
+        if (notes & LA) {
+            std::cout << "LA" << " ";
+        }
+        if (notes & SI) {
+            std::cout << "SI" << " ";
+        }
+    }
     return 0;
 }
